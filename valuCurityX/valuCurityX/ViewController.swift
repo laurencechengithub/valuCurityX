791f0d3c21368c3000f2f229fe24b1117d2d8476
laurence <laurencembp2>
2022-06-23 23:45:15 +0800

    init project

---
+++ valuCurityX/valuCurityX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  valuCurityX
+//
+//  Created by LaurenceMBP2 on 2022/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
